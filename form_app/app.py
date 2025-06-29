d4655347d3f1c988b1e1ade6d9c4d8d77d0c6193
Shen <xjxjcldk9@gmail.com>
2025-06-29 20:34:41 +0800

    重構flask

--- form_app/app.py
+++ form_app/app.py
@@ -1,8 +1,28 @@
-from flask import Flask, render_template, redirect, url_for, request
 import datetime
+import os
 import urllib.parse
 
+import psycopg
+from flask import Flask, redirect, render_template, request, session, url_for
+from psycopg.rows import dict_row
+
 app = Flask(__name__)
+app.secret_key = 'secret'
+DB = os.getenv('DB')
+
+
+def get_token_matching(token, conn):
+    with conn.cursor(row_factory=dict_row) as curr:
+        stmt = "select id, subject_id, object_id from matching where access_token = %s;"
+        result = curr.execute(stmt, (token,)).fetchone()
+        return result
+
+
+def get_obj_name(member_id, conn):
+    with conn.cursor() as curr:
+        stmt = "select name from member where id = %s;"
+        result = curr.execute(stmt, (member_id,)).fetchone()[0]
+        return result
 
 
 @app.route('/submit-places', methods=['GET', 'POST'])
@@ -40,24 +60,59 @@ def confirm_places():
     return redirect(url_for('thank_you'))
 
 
-@app.route('/confirm', methods=['GET'])
-def confirm():
-    # Render the confirmation page (modifiable via Jinja2)
-    return render_template('confirm.html', message="Do you want to confirm this action?")
+# TODO: 每一個操作都在這裡，出發
+@app.route('/<token>/<action>', methods=['GET'])
+def router(token, action):
+    # 先在這邊解析token，並導引至正確的動作
+    with psycopg.connect(DB) as conn:
+        matching_info = get_token_matching(token, conn)
+        if matching_info:
+            session['matching_info'] = matching_info
+            obj_name = get_obj_name(matching_info['object_id'], conn)
+            session['obj_name'] = obj_name
+
+            if action == 'invitation':
+                return redirect(url_for('invitation'))
+
+            else:
+                return redirect(url_for('error'))
+
+        else:
+            return redirect(url_for('error'))
+
+
+@app.route('/invitation', methods=['GET'])
+def invitation():
+    pass
 
 
 @app.route('/confirm', methods=['POST'])
 def confirm_post():
     # Here you record the confirmation
-    with open("confirmation_log.txt", "a") as f:
-        f.write(f"Confirmed at {datetime.datetime.now()}\n")
+    matching_info = session.get('matching_info')
+    with psycopg.connect(DB) as conn:
+        with conn.cursor() as curr:
+            # check state，如果有人不在正確的state，本次操作取消
+            state_stmt = "select TRUE from matching where id = %s and current_state <> 'invitation_sending';"
+            check = curr.execute(
+                state_stmt, (matching_info['id'],)).fetchone()
+            if check:
+                redirect(url_for('error'))
+            stmt = "update matching set current_state = 'invitation_waiting' where id=%s;"
+            curr.execute(
+                stmt, (matching_info['id'],))
 
     return redirect(url_for('thank_you'))
 
 
+@app.route('/error', methods=['GET'])
+def error():
+    return render_template('error.html')
+
+
 @app.route('/thank-you')
 def thank_you():
-    return render_template('thank_you.html')
+    return render_template('thank_you.html', message="您的like已經傳送給對方👍")
 
 
 if __name__ == '__main__':
