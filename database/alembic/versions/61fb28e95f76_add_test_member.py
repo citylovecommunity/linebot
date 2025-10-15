44130ff9992896a6f36c73229ffd4fd3d9945a35
Shen <xjxjcldk9@gmail.com>
2025-10-15 10:37:25 +0800

    弄錯

--- database/alembic/versions/61fb28e95f76_add_test_member.py
+++ database/alembic/versions/61fb28e95f76_add_test_member.py
@@ -1,8 +1,8 @@
-"""book_time_to tz
+"""add test member
 
-Revision ID: e2f525055ae3
-Revises: 7614fdaab3a6
-Create Date: 2025-10-08 22:30:58.346121
+Revision ID: 61fb28e95f76
+Revises: e2f525055ae3
+Create Date: 2025-10-14 14:52:02.535648
 
 """
 from typing import Sequence, Union
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = 'e2f525055ae3'
+revision: str = '61fb28e95f76'
 down_revision: Union[str, Sequence[str], None] = '7614fdaab3a6'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
@@ -22,23 +22,15 @@ def upgrade() -> None:
     """Upgrade schema."""
     op.execute(
         """
-        alter table matching
-        drop book_time;
-        
-        alter table matching
-        add book_time timestamp with time zone;
+        alter table member
+        add column is_test boolean;
         """
     )
 
 
 def downgrade() -> None:
     """Downgrade schema."""
-    op.execute(
-        """
-        alter table matching
-        drop book_time;
-        
-        alter table matching
-        add book_time time;
-        """
-    )
+    """
+    alter table member
+    drop column is_test;
+    """
