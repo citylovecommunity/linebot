f89f4af5320f1864eabb8af37f72d32b76d8fd8e
Shen <xjxjcldk9@gmail.com>
2026-02-11 00:33:11 +0800

    用preping

--- shared/shared/database/session_maker.py
+++ shared/shared/database/session_maker.py
@@ -3,5 +3,6 @@ from sqlalchemy.orm import Session, sessionmaker
 
 
 def get_session_factory(database_url: str) -> Session:
-    engine = create_engine(database_url)
+    engine = create_engine(database_url,
+                           pool_pre_ping=True)
     return sessionmaker(autocommit=False, autoflush=False, bind=engine)
